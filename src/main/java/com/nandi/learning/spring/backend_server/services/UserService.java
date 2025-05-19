477e577ff99a258fcd7dae079f664cccbac645bb
Hari-repo <hariharanuk810@gmail.com>
2025-05-19 23:00:04 +0530

    Devtools added in dependency

--- src/main/java/com/nandi/learning/spring/backend_server/services/UserService.java
+++ src/main/java/com/nandi/learning/spring/backend_server/services/UserService.java
@@ -3,11 +3,13 @@ package com.nandi.learning.spring.backend_server.services;
 import com.nandi.learning.spring.backend_server.domain.Users;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class UserService {
 
     public String sayHello(){
-        return "Gotcha Spring boot";
+        return "Gotcha Spring boot : ============= " + LocalDateTime.now().toString();
     }
 
     public Users getUserById(int x){
